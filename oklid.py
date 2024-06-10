74223d59438eab0654af2e550a741ed6cb6fedee
unknown <sudenazdemir>
2024-06-10 19:14:11 +0300

    yeniden adlandırıldı.

--- oklid.py
+++ oklid.py
@@ -4,18 +4,18 @@ import math
 points = [(3, 5), (6, 20), (8, 12), (5, 8)]  
 
 # Öklid Mesafesi İçin  Fonksiyon 
-def oklidMesafe(point1, point2):
+def euclideanDistance(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 # Mesafelerin Hesaplanması
-mesafeler = []
+distances = []
 for i in range(len(points)):
     for j in range(i+1, len(points)):
-        mesafe = oklidMesafe(points[i], points[j])
-        mesafeler.append(mesafe)
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
 
 # Minimum Mesafenin Bulunması
-min_mesafe = min(mesafeler)
-print("Minimum mesafe:", min_mesafe)
+min_distance = min(distances)
+print("Minimum mesafe:", min_distance)
